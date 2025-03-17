9d06ce6d44d3bcc68da75fd732c3dfaa51a1c829
DACS <dcasass@ulasalle.edu.pe>
2025-03-16 21:13:47 -0500

    Creacion rock_paper_scc

---
+++ Asign_1/Rock_Paper_Scc.kt
@@ -0,0 +1,10 @@
+// Rock, Paper and shears
+// Autor: Dan Casas
+// Fecha creación: 16/03/2025
+// Fecha última modificación: 16/03/2025
+
+fun main() {
+    println("Elige rock, paper or shears:")
+    val eleccion = readLine()
+    print("$eleccion")
+}
