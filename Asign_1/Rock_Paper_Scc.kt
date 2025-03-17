f59516652e270f4bb51c374fd3a96708a1b79b1f
DACS <dcasass@ulasalle.edu.pe>
2025-03-16 21:17:56 -0500

    Prueba y añadir random

--- Asign_1/Rock_Paper_Scc.kt
+++ Asign_1/Rock_Paper_Scc.kt
@@ -3,8 +3,15 @@
 // Fecha creación: 16/03/2025
 // Fecha última modificación: 16/03/2025
 
+import kotlin.random.Random
+
 fun main() {
     println("Elige rock, paper or shears:")
     val eleccion = readLine()
     print("$eleccion")
+
+    val opciones = listOf("rock", "paper", "shears")
+    val eleccionPc = opciones[Random.nextInt(opciones.size)]
+    println("Computadora eligió: $eleccionPc")
+
 }
