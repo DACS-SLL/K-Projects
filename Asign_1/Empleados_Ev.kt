97d14e56e01a349cb4a99df3f92324b626ef3442
DACS <dcasass@ulasalle.edu.pe>
2025-03-16 21:03:02 -0500

    Función main

--- Asign_1/Empleados_Ev.kt
+++ Asign_1/Empleados_Ev.kt
@@ -2,3 +2,12 @@
 // Autor: Dan Casas
 // Fecha creación: 16/03/2025
 // Fecha última modificación: 16/03/2025
+
+fun main() {
+    println("Ingrese su salario mensual:")
+    val salario = readLine()
+    println("Ingrese su puntuación:")
+    val puntuacion = readLine()
+
+    println("$salario y $puntuacion")
+}
