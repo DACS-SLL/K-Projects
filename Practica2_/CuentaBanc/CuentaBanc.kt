dacfb84f3056f4cf13f3d4f4bdd7a32347e4a803
DACS <dcasass@ulasalle.edu.pe>
2025-03-23 21:44:49 -0500

    Creación de clase CuentaBancaria

--- Practica2_/CuentaBanc/CuentaBanc.kt
+++ Practica2_/CuentaBanc/CuentaBanc.kt
@@ -3,3 +3,20 @@
 // Fecha creación: 23/03/2025
 // Fecha última modificación: 23/03/2025
 
+// Constructor de clase
+class CuentaBancaria {
+    private var saldo: Double = 0.0
+    private var limiteRet: Double = 1000.0
+
+    // Getter y Setter para saldo
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(nuevoSaldo: Double) {
+        if (nuevoSaldo >= 0) {
+            saldo = nuevoSaldo // No agrega saldo, establece uno nuevo
+        } else {
+            println("Error: El saldo no puede ser negativo.")
+        }
+    }
