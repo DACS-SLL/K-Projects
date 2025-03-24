406e8b9f49003ed70fd7353d3b0982adec9d6778
DACS <dcasass@ulasalle.edu.pe>
2025-03-23 23:41:52 -0500

    Clase circulo y rectangulo con pruebas

--- Practica2_/Formas/Formas.kt
+++ Practica2_/Formas/Formas.kt
@@ -23,8 +23,38 @@ class Cuadrado(private val lado: Double) : Shape() {
     }
 }
 
+class Circulo(private val radio: Double) : Shape() {
+    override fun calcularArea(): Double {
+        return Math.PI * radio * radio
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 2 * Math.PI * radio
+    }
+}
+
+class Rectangulo(private val largo: Double, private val ancho: Double) : Shape() {
+    override fun calcularArea(): Double {
+        return largo * ancho
+    }
+
+    override fun calcularPerimetro(): Double {
+        return 2 * (largo + ancho)
+    }
+}
+
+//Pruebas
 fun main() {
     val cuadrado = Cuadrado(5.0)
     println("Calculando Cuadrado...")
     cuadrado.imprimirResultados()
+
+    val circulo = Circulo(3.0)
+    println("Calculando Circulo...")
+    circulo.imprimirResultados()
+
+    val rectangulo = Rectangulo(4.0, 6.0)
+    println("Calculando Rectangulo...")
+    rectangulo.imprimirResultados()
+}
 }
