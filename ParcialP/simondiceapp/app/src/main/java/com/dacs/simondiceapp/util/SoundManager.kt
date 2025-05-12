a5ce61d4ebdc3c197adbe8da2b79bdd6ba04816a
Daniel Casas :) <dcasass@ulasalle.edu.pe>
2025-05-11 21:31:51 -0500

    Paso 5: Agregaremos un soundManager para poder reproducir nuestros sonidos :)

--- ParcialP/simondiceapp/app/src/main/java/com/dacs/simondiceapp/util/SoundManager.kt
+++ ParcialP/simondiceapp/app/src/main/java/com/dacs/simondiceapp/util/SoundManager.kt
@@ -1,2 +1,30 @@
 package com.dacs.simondiceapp.util
 
+import android.content.Context
+import android.media.MediaPlayer
+
+object SoundManager {
+    private var mediaPlayer: MediaPlayer? = null
+
+    /**
+     * Reproduce un sonido desde raw y libera el recurso al finalizar
+     */
+    fun playSound(context: Context, resId: Int, onComplete: (() -> Unit)? = null) {
+        stopSound()
+        mediaPlayer = MediaPlayer.create(context, resId)
+        mediaPlayer?.setOnCompletionListener {
+            stopSound()
+            onComplete?.invoke()
+        }
+        mediaPlayer?.start()
+    }
+
+    /**
+     * Detiene y libera el sonido actual
+     */
+    fun stopSound() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+}
