babdf950152f005d34aa083510a4ab8f14fae0de
Daniel Casas :) <dcasass@ulasalle.edu.pe>
2025-05-11 21:36:29 -0500

    Paso 6: Agregaremos un manager de preferencias para poder guardar volumen, sonido de la app

--- ParcialP/simondiceapp/app/src/main/java/com/dacs/simondiceapp/data/PreferencesManager.kt
+++ ParcialP/simondiceapp/app/src/main/java/com/dacs/simondiceapp/data/PreferencesManager.kt
@@ -1,4 +1,16 @@
 package com.dacs.simondiceapp.data
 
-class PreferencesManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesManager(context: Context) {
+    private val prefs: SharedPreferences = context.getSharedPreferences("user_settings", Context.MODE_PRIVATE)
+
+    fun setVolumeEnabled(enabled: Boolean) {
+        prefs.edit().putBoolean("volume", enabled).apply()
+    }
+
+    fun isVolumeEnabled(): Boolean {
+        return prefs.getBoolean("volume", true)
+    }
 }
